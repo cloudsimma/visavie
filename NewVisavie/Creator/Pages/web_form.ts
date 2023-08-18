f983c5edaffed30381078c05400dabb460e940c8
cloudsimma <102286268+cloudsimma@users.noreply.github.com>
2023-08-18 21:25:53 +0530

    Create web_form.ts

---
+++ NewVisavie/Creator/Pages/web_form.ts
@@ -0,0 +1,34 @@
+<%{
+	%>
+<div id="zf_div_r0_u4A-bU30kPPTme-of97Qsyx5tF3um99g4b6-JX4g"></div><script type="text/javascript">(function() {
+try{
+var f = document.createElement("iframe");
+f.src = 'https://forms.zohopublic.com/pocportal/form/Test1/formperma/r0_u4A-bU30kPPTme-of97Qsyx5tF3um99g4b6-JX4g?zf_rszfm=1';
+f.style.border="none";
+f.style.height="355px";
+f.style.width="90%";
+f.style.transition="all 0.5s ease";
+var d = document.getElementById("zf_div_r0_u4A-bU30kPPTme-of97Qsyx5tF3um99g4b6-JX4g");
+d.appendChild(f);
+window.addEventListener('message', function (){
+var evntData = event.data;
+if( evntData && evntData.constructor == String ){
+var zf_ifrm_data = evntData.split("|");
+if ( zf_ifrm_data.length == 2 ) {
+var zf_perma = zf_ifrm_data[0];
+var zf_ifrm_ht_nw = ( parseInt(zf_ifrm_data[1], 10) + 15 ) + "px";
+var iframe = document.getElementById("zf_div_r0_u4A-bU30kPPTme-of97Qsyx5tF3um99g4b6-JX4g").getElementsByTagName("iframe")[0];
+if ( (iframe.src).indexOf('formperma') > 0 && (iframe.src).indexOf(zf_perma) > 0 ) {
+var prevIframeHeight = iframe.style.height;
+if ( prevIframeHeight != zf_ifrm_ht_nw ) {
+iframe.style.height = zf_ifrm_ht_nw;
+}
+}
+}
+}
+}, false);
+}catch(e){}
+})();</script>
+<%
+
+}%>
