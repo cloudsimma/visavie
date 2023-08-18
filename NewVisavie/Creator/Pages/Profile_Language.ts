10ab4891cf247b479312eb013ed9c3e57d9b4b6c
cloudsimma <102286268+cloudsimma@users.noreply.github.com>
2023-08-18 21:22:50 +0530

    Create Profile_Language.ts

---
+++ NewVisavie/Creator/Pages/Profile_Language.ts
@@ -0,0 +1,49 @@
+<%{
+	%>
+<html>
+<style>
+.box
+{
+// width: 500px;
+height: 300px;
+// background: rgba(232, 225, 225, 0.5);
+// color: #0a0a0a;
+// top:50%;
+// left:50%;
+// position:absolute;
+// transform: translate(-50%,-50%);
+// box-sizing: border-box;
+// align-items: center;
+}
+</style>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/js/all.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+  <div class="container">
+  <h4><span class="dark form-check d-flex justify-content-center">Type of language</span></h4>
+    <br>
+    <div class="form-check d-flex justify-content-center" style="padding-top: 30px;">
+        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+        <label class="form-check-label" for="exampleRadios1"> English</label>
+      </div>
+      <br>
+      <div class="form-check d-flex justify-content-center">
+        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+        <label class="form-check-label" for="exampleRadios2"> French </label>
+      </div >
+      <div style="padding-top: 50px;"class="form-check d-flex justify-content-center"> 
+<a class="btn btn-success" target="_blank" href= "https://creatorapp.zohopublic.com/lion_visavie/visavie/page-perma/Client_Profile_Language/vmnmQJVT3jqgprSU2bOgRX4fBz3Rs3xQjJDxSnJtmtKWf4yjNtFCMDrkK0hOCnKZx6KCwb3NFhJvY4esQYprJKm3htN1pRT3qZFU?Language=English">Submit</a>
+    </div>
+    </div>
+</body>
+</html>
+<%
+
+}%>
