2579d005a85b7b657f4e24f3b5a1784ab36d4706
cloudsimma <102286268+cloudsimma@users.noreply.github.com>
2023-08-18 14:52:16 +0530

    Create HomecareDealnumberOncreate.ts

---
+++ NewVisavie/ClientScript/DémarcheSoins/HomecareDealnumberOncreate.ts
@@ -0,0 +1,8 @@
+var field_obj = ZDK.Page.getField('Home_Care_Number');
+field_obj.setReadOnly(true);
+
+var client1 = ZDK.Page.getField('Client_1_number');
+client1.setReadOnly(true);
+
+var client2 = ZDK.Page.getField('Client_2_number');
+client2.setReadOnly(true);
