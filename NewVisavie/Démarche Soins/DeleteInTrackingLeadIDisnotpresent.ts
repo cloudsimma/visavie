bb90a2296ff6220299c865c84da7acd453bf11e5
cloudsimma <102286268+cloudsimma@users.noreply.github.com>
2023-08-18 13:50:36 +0530

    Create DeleteInTrackingLeadIDisnotpresent.ts

---
+++ NewVisavie/Démarche Soins/DeleteInTrackingLeadIDisnotpresent.ts
@@ -0,0 +1,27 @@
+try 
+{
+	if(DemarchesoindID != null && trackID != null)
+	{
+		trackInfo = zoho.crm.getRecordById("Tracking",trackID);
+		if(trackInfo.get("id") != null)
+		{
+			newTRackID = trackInfo.get("id");
+			deleteRecordMap = Map();
+			deleteRecordMap.put("module","Tracking");
+			deleteRecordMap.put("id",newTRackID);
+			deleteResp = zoho.crm.invokeConnector("crm.delete",deleteRecordMap);
+		}
+	}
+}
+catch (e)
+{
+	appName = "visavie";
+	ownerName = "lion_visavie";
+	formName = "Developer_Log";
+	dataMap = Map();
+	dataMap.put("Module","Home_Care_Deal");
+	dataMap.put("Process_Description","Delete Record in Tracking When Lead ID is not present");
+	dataMap.put("In_Data",DemarchesoindID);
+	dataMap.put("Out_Response",e);
+	deleteResponse = zoho.creator.createRecord(ownerName,appName,formName,dataMap,Map(),"zoho_mail");
+}
