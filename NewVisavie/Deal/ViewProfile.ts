56042cf97905cb6716abd9b984904e46e3c5a985
cloudsimma <102286268+cloudsimma@users.noreply.github.com>
2023-08-18 14:38:37 +0530

    Create ViewProfile.ts

---
+++ NewVisavie/Deal/ViewProfile.ts
@@ -0,0 +1,1 @@
+https://creatorapp.zohopublic.com/lion_visavie/visavie/page-embed/View_Profile1/7ZDn5WEaZSYEbfjS7hwjayfGFKVw3W0zF9JV3SaAD1OFeHg99z3u8nt0Dv2vJBean5WTR9JHvPSuUASns2huFhFQFMufbAkVumZy?dealID=${Démarches/Deals.Démarche Id}&fromCRM=true
